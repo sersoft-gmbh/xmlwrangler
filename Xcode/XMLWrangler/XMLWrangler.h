8ce7818ae911084c49c57c105a89d07c8435bcf7
Florian Friedrich <ffried@me.com>
2019-01-18 18:49:35 +0100

    Adjust build settings

--- Xcode/XMLWrangler/XMLWrangler.h
+++ Xcode/XMLWrangler/XMLWrangler.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double XMLWranglerVersionNumber;
 FOUNDATION_EXPORT const unsigned char XMLWranglerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <XMLWrangler/PublicHeader.h>
-
-
