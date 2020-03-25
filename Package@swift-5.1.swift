e1a804a08c8c3b0fe10c85547022f170cda211ed
Florian Friedrich <ffried@me.com>
2020-03-25 13:46:48 +0100

    Add support for Swift 5.2

--- Package@swift-5.1.swift
+++ Package@swift-5.1.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 
 import PackageDescription
 
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SemVer", url: "https://github.com/sersoft-gmbh/semver.git", from: "2.3.1"),
+        .package(url: "https://github.com/sersoft-gmbh/semver.git", from: "2.3.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
